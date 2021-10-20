96b354136f2e38b4da73a918610af409bb5d37c4
Rikiya Yamashita <rikiya@stanford.edu>
2021-10-20 12:27:00 -0700

    Update extract_measurements.py

--- extract_measurements.py
+++ extract_measurements.py
@@ -1,14 +1,3 @@
-"""
-Software Requirements (this code was developed and tested in the following settings)
-    OS: Ubuntu 18.04
-    GPU: Nvidia GeForce RTX 2080 Ti
-    Dependencies:
-        - python (3.7.9)
-        - pandas (1.1.3)
-        - pytorch (1.6.0)
-        - transformers (3.3.1)
-"""
-
 import os
 from datetime import datetime
 
